85f45b5a8abaa1cfc5dc2f7347e72f4467c74ef9
Bruno Gonzales <brunogonzales1129@gmail.com>
2020-04-16 18:23:23 -0500

    Add static folder

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const posts = require("./routes/api/posts");
+const posts = require('./routes/api/posts')
 
-app.use("/api/posts", posts);
+app.use('/api/posts', posts)
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'))
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  })
+}
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
